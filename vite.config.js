4fa5b2d4e35212b9849e596232df93caa283a193
Sam Almohanna <webdevsumayah@gmail.com>
2025-11-16 16:24:19 -0500

    chore: vite config.

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    base: "/github-game-off-2025/",
+    build: {
+        outDir: "dist",
+        assetsDir: "assets",
+        emptyOutDir: true,
+        chunkSizeWarningLimit: 1500,
+    },
+});
